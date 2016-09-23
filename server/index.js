374517cc4abfbea7174afc4e02087cb226a6ed20
thulioph <thulioph@gmail.com>
2016-09-23 19:21:12 -0300

    Updates the server.

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ var server = express().use(function(req, res) {
   console.log('listening on port: ', port);
 });
 
-server.use('/styles', express.static(path.join(__dirname, '../client/dist/styles'));
-server.use('/scripts', express.static(path.join(__dirname, '../client/dist/scripts'));
-server.use('/images', express.static(path.join(__dirname, '../client/dist/images'));
+server.use('/styles', express.static(__dirname, '../client/dist/styles');
+server.use('/scripts', express.static(__dirname, '../client/dist/scripts');
+server.use('/images', express.static(__dirname, '../client/dist/images');
 
 var io = socketIo(server);
 
