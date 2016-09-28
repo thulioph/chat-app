fcdffc8f5fcfdeb1b7ed1372c13b6cd8c321e9a0
thulioph <thulioph@gmail.com>
2016-09-28 01:19:53 -0300

    Initialize socket in about.js and creates a function to dispatch events.

--- client/app/scripts/controllers/about.js
+++ client/app/scripts/controllers/about.js
@@ -10,7 +10,7 @@
    * Controller of the chatAppApp
    */
 
-  function AboutCtrl($rootScope, $location) {
+  function AboutCtrl($rootScope, $location, SocketService) {
     var vm;
 
     vm = this;
@@ -18,16 +18,45 @@
 
     vm.form = {};
 
+    Init();
+
     // ====
 
+    function Init() {
+      vm.socket = SocketService.init();
+    }
+
     function signUp() {
+      var guest, user;
+
+
       if (vm.form.guest) {
         $rootScope.guest = true;
+
+        guest = {
+          'username': 'Alguém',
+          'timestamp': new Date().getTime()
+        };
+
+        dispatch_event(guest);
       } else {
         $rootScope.user = vm.form;
+
+        user = {
+          'username': vm.form.username,
+          'event': vm.form.event,
+          'timestamp': new Date().getTime()
+        };
+
+        dispatch_event(user);
       }
 
       $location.path('/main');
+    }
+
+    function dispatch_event(obj) {
+      // console.log('novo_usuario ', obj);
+      vm.socket.emit('novo_usuario', obj);
     }
   }
 
@@ -35,6 +64,6 @@
   .module('chatAppApp')
   .controller('AboutCtrl', AboutCtrl);
 
-  AboutCtrl.$inject = ['$rootScope', '$location'];
+  AboutCtrl.$inject = ['$rootScope', '$location', 'SocketService'];
 
 })();
