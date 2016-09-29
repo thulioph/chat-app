111ed287f183ddc5d10630030fe3f2734315a683
thulioph <thulioph@gmail.com>
2016-09-28 22:09:24 -0300

    Change node port.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
-var express, app, http, io;
+var express, app, http, io, port;
 
 express = require('express');
 app = express();
 http = require('http').Server(app);
 io = require('socket.io')(http);
+port = process.env.PORT || 3000;
 
+// servindo os arquivos
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
@@ -52,6 +54,7 @@ io.on('connection', function(socket){
   })
 });
 
-http.listen(3000, function() {
+// levantando o server
+http.listen(port, function() {
   console.log('Server is running on *:3000');
 });
