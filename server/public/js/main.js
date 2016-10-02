79ecf72b0653de59e3be4aee8ac2e149292a87d5
thulioph <thulioph@gmail.com>
2016-10-02 13:04:30 -0300

    Adds main.js file.

---
+++ server/public/js/main.js
@@ -0,0 +1,20 @@
+//
+// Main
+//
+
+$('form').submit(function(){
+  var obj;
+
+  obj = {
+    'user': $('#user').val(),
+    'msg': $('#msg').val(),
+    'timestamp': new Date().getTime()
+  };
+
+  socket.emit('chat message', obj);
+
+  $('#user').val('');
+  $('#msg').val('');
+
+  return false;
+});
